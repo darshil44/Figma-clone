aef8cb65f19844a5f6c9ada36e4ff464dd410909
Darshil Dobariya <89260736+darshil44@users.noreply.github.com>
2025-01-07 11:44:12 +0530

    Update App.tsx

--- app/App.tsx
+++ app/App.tsx
@@ -2,7 +2,7 @@
 
 import { fabric } from "fabric";
 import { useEffect, useRef, useState } from "react";
-
+ 
 import { useMutation, useRedo, useStorage, useUndo } from "@/liveblocks.config";
 import {
   handleCanvaseMouseMove,
