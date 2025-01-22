b4155c8f5bcae727b6fc48aef34ae1cb07b185dd
Darshil Dobariya <89260736+darshil44@users.noreply.github.com>
2025-01-22 14:30:00 +0530

    Update index.ts

--- constants/index.ts
+++ constants/index.ts
@@ -1,6 +1,6 @@
 export const COLORS = ["#DC2626", "#D97706", "#059669", "#7C3AED", "#DB2777"];
  
-export const shapeElements = [
+export const shapeElements = [ 
   { 
     icon: "/assets/rectangle.svg",
     name: "Rectangle", 
